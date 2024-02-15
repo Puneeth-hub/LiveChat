05ab1d254c6d7e6b039f39b0960571d5e044b2e3
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-02-15 15:03:54 +0530

    live chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState,useRef } from 'react';
 import './App.css';
+import { Auth } from './components/Auth';
+import Cookies from 'universal-cookie';
+import { Chat } from './components/Chat';
+import {signOut} from 'firebase/auth'
+import {auth} from './firebase.config'
+const cookies = new Cookies();
+
 
 function App() {
+  const [isAuth,setIsAuth] = useState(cookies.get("auth-token")); 
+  const [room,setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);  
+
+  const signUserOut = async () =>{
+      await signOut(auth);
+      cookies.remove("auth-token");
+      setIsAuth(false); 
+      setRoom(null);
+  }
+
+  if(!isAuth){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth setIsAuth={setIsAuth}/>
     </div>
   );
+  }
+
+  return(
+     <>
+      {room ? (
+        <Chat room={room}/>
+      ) : (
+        <div className='room'>
+           <label>Enter room Name:</label> 
+           <input ref={roomInputRef}/>
+           <button onClick={()=>setRoom(roomInputRef.current.value)}>Enter </button>
+        </div>
+      )}
+      <div>
+          <button onClick={signUserOut} className='sign-out'> Sign out </button>
+      </div>
+     </>
+  )
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8MQYO8mI9vCsJx_wDhwDNlfnHb6mTka0",
+  authDomain: "livechat-515ff.firebaseapp.com",
+  projectId: "livechat-515ff",
+  storageBucket: "livechat-515ff.appspot.com",
+  messagingSenderId: "786713995186",
+  appId: "1:786713995186:web:408da368acaf256412e6ab",
+  measurementId: "G-KKEVKM4VBH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); 
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app)
